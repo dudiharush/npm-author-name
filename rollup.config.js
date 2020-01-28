abe6dc81666f627479adb7b326057d9816685049
David Harush <davidharush@gmail.com>
2020-01-28 17:06:46 +0200

    add cli

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+import shebang from 'rollup-plugin-add-shebang';
 
 const extensions = [
   '.js', '.jsx', '.ts', '.tsx',
@@ -33,6 +34,26 @@ export default [{
     file: pkg.module,
     format: 'es',
    }
+],
+},
+{
+  input: 'cli.ts',
+  external,
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+    // Compile TypeScript/JavaScript files
+    babel({ extensions, include: ['src/cli.ts'] }),
+    shebang({include: pkg.bin})
+  ],
+  external,
+
+  output: [{
+    file: pkg.bin,
+    format: 'cjs',
+  }
 ],
 }
 ];
