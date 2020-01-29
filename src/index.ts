b3eae809ca972d92efa7091c1fdfabe1954d33bf
David Harush <davidharush@gmail.com>
2020-01-29 10:09:48 +0200

    change key

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import shell from 'shelljs'
 import readGitUsername from 'git-user-name'
 
 export function setAuthorName(author: string) {
-  shell.exec(`npm config set init-author-name "${author}"`, { silent: true })
+  shell.exec(`npm config set init.author.name "${author}"`, { silent: true })
 }
 
 export function getAuthorName() {
   let author: string | null = ''
 
-  author = shell.exec('npm config get init-author-name', { silent: true }).stdout.trim()
+  author = shell.exec('npm config get init.author.name', { silent: true }).stdout.trim()
   if (author) return author
   author = readGitUsername()
   if (author) {
